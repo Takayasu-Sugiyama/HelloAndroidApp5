243a85393bf14fd110e6e26e737848dfdddc7e9a
Takayasu-Sugiyama <washashasha_sugiyama@yahoo.co.jp>
2020-03-02 22:10:55 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/Takayasu/Sugiyama/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.Takayasu.Sugiyama
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
